aa00a944a6cb3211621d76a39781cc4bf0ab623b
wi-seong-cheol <tjd9248@naver.com>
2021-12-22 02:28:41 +0900

    Initial Commit

---
+++ cnn_opencl/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  cnn_opencl
+//
+//  Created by wi_seong on 2021/12/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
